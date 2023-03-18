1349074ece3d51aa31c03c5651819dce5a01790b
leoobarbosa2 <leonardobarbosa@MacBook-Pro-de-Leonardo.local>
2023-03-18 20:47:28 -0300

    feat: Card Component

---
+++ src/components/Card/index.tsx
@@ -0,0 +1,29 @@
+import { CardContainer, CardDescription, CardPublishedDate, CardTitle, LinkContainer } from "./styles";
+import { formatDistanceToNow } from 'date-fns';
+import { ptBR } from 'date-fns/locale';
+
+interface CardProps {
+    title: string;
+    description: string;
+    publishedDate: string,
+    linkTo: string;
+  }
+   
+  export const Card = ({ title, description, publishedDate, linkTo }: CardProps) => {
+    const formattedDate = formatDistanceToNow(new Date(publishedDate), {
+        locale: ptBR,
+        addSuffix: true
+    })
+
+    return (
+      <LinkContainer href={linkTo}>
+        <CardContainer>
+            <CardPublishedDate>
+                {formattedDate}
+            </CardPublishedDate>
+            <CardTitle>{title}</CardTitle>
+            <CardDescription>{description}</CardDescription>
+        </CardContainer>
+      </LinkContainer>
+    )
+  }
---
+++ src/components/Card/styles.ts
@@ -0,0 +1,50 @@
+import styled from "styled-components";
+import Link from 'next/link';
+
+export const LinkContainer = styled(Link)`
+    display: block;
+    text-decoration: none;
+    
+    &:hover {
+        h2 {
+            color: ${({ theme }) => theme.colors.secondary};
+        }
+    }
+
+    & + & {
+        margin-top: 1.25rem;
+        
+    }
+
+    &:last-child > div {
+        border: none;
+    }
+`;
+
+export const CardContainer = styled.div`
+    min-height: 160px;
+    padding: 1.25rem 0;
+    border-bottom: 1px solid ${({ theme }) => theme.colors.secondary};
+
+    &:last-child{
+    }
+`;
+
+export const CardPublishedDate = styled.span`
+    font-size: 0.875rem;
+    color: ${({ theme }) => theme.colors.secondary};
+    font-weight: bold;
+`;
+
+export const CardTitle = styled.h2`
+    transition: color 0.4s;
+    font-size: 2rem;
+    font-weight: bold;
+    color: ${({ theme }) => theme.colors.primary};
+`;
+
+export const CardDescription = styled.p`
+    margin: 0.5rem 0;
+    line-height: 1.6;
+    color: ${({ theme }) => theme.colors["text-dark"]};
+`;
