4cc94b685fd05792d343a6ff8746deda822d3af0
leoobarbosa2 <leonardobarbosa@MacBook-Pro-de-Leonardo.local>
2023-03-18 12:25:02 -0300

    feat: Defining App font with next/font

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -4,12 +4,20 @@ import type { AppProps } from "next/app";
 
 import { PrismicPreview } from "@prismicio/next";
 import { repositoryName } from "../../prismicio";
+import { Roboto } from '@next/font/google'
+
+const roboto = Roboto({
+  subsets: ['latin'],
+  weight: ['400', '700']
+})
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
     <PrismicProvider internalLinkComponent={(props) => <Link {...props} />}>
       <PrismicPreview repositoryName={repositoryName}>
+        <div className={roboto.className}>
         <Component {...pageProps} />
+        </div>
       </PrismicPreview>
     </PrismicProvider>
   );
