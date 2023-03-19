c97a4661456faf93cc0f3ef3ecd92cd1aee9f30b
leoobarbosa2 <leonardobarbosa@MacBook-Pro-de-Leonardo.local>
2023-03-19 16:57:51 -0300

    refactor: removing unused variable

--- src/styles/default.ts
+++ src/styles/default.ts
@@ -2,7 +2,6 @@ export const defaultTheme = {
   colors: {
     primary: '#3F4E4F',
     secondary: '#A27B5C',
-    tertiary: '3F4E4F',
     light: '#DCD7C9',
     'gray-light': '#E1E1E6',
     'text-dark': '#29292E',
