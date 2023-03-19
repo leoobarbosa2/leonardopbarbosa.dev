3ff1395555eee9d050ef91b425e8cc8841e7061a
leoobarbosa2 <leonardobarbosa@MacBook-Pro-de-Leonardo.local>
2023-03-19 09:10:03 -0300

    feat: Adding custom favicon

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,33 +1,37 @@
-import Link from "next/link";
-import { PrismicProvider } from "@prismicio/react";
-import type { AppProps } from "next/app";
+import Link from 'next/link'
+import Head from 'next/head'
+import { PrismicProvider } from '@prismicio/react'
+import type { AppProps } from 'next/app'
 
-import { PrismicPreview } from "@prismicio/next";
-import { repositoryName } from "../../prismicio";
+import { PrismicPreview } from '@prismicio/next'
+import { repositoryName } from '../../prismicio'
 import { Roboto } from 'next/font/google'
-import { GlobalStyle } from "@/styles/global";
-import { defaultTheme } from "@/styles/default";
-import { ThemeProvider } from "styled-components";
-import { DefaultLayout } from "@/templates/default";
+import { GlobalStyle } from '@/styles/global'
+import { defaultTheme } from '@/styles/default'
+import { ThemeProvider } from 'styled-components'
+import { DefaultLayout } from '@/templates/default'
 
 const roboto = Roboto({
   subsets: ['latin'],
-  weight: ['400', '700']
+  weight: ['400', '700'],
 })
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
     <PrismicProvider internalLinkComponent={(props) => <Link {...props} />}>
-        <ThemeProvider theme={defaultTheme}>
-      <PrismicPreview repositoryName={repositoryName}>
-        <div className={roboto.className}>
-          <DefaultLayout>
-            <Component {...pageProps} />
-          </DefaultLayout>
-        </div>
-      </PrismicPreview>
-      <GlobalStyle />
-        </ThemeProvider>
+      <Head>
+        <link rel="shortcut icon" href="favicon.png" type="image/x-icon" />
+      </Head>
+      <ThemeProvider theme={defaultTheme}>
+        <PrismicPreview repositoryName={repositoryName}>
+          <div className={roboto.className}>
+            <DefaultLayout>
+              <Component {...pageProps} />
+            </DefaultLayout>
+          </div>
+        </PrismicPreview>
+        <GlobalStyle />
+      </ThemeProvider>
     </PrismicProvider>
-  );
+  )
 }
