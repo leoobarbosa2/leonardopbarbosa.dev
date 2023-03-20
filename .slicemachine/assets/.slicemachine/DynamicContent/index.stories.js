6af6645ca03b9902126d0b45ca42557b29564c57
leoobarbosa2 <leonardobarbosa@MacBook-Pro-de-Leonardo.local>
2023-03-20 19:18:47 -0300

    feat: Supporting <ul> elements as a Prismic Slice

--- .slicemachine/assets/.slicemachine/DynamicContent/index.stories.js
+++ .slicemachine/assets/.slicemachine/DynamicContent/index.stories.js
@@ -1,27 +1,9 @@
-import MyComponent from '../../../DynamicContent'
+import MyComponent from '../../../DynamicContent';
 
 export default {
-  title: '.slicemachine/DynamicContent',
+  title: '.slicemachine/DynamicContent'
 }
 
-export const _Default = () => (
-  <MyComponent
-    slice={{
-      variation: 'default',
-      version: 'sktwi1xtmkfgx8626',
-      items: [{}],
-      primary: {
-        content: [
-          {
-            type: 'paragraph',
-            text: 'Commodo aliquip ea voluptate nostrud esse quis adipisicing ad reprehenderit eiusmod in.',
-            spans: [],
-          },
-        ],
-      },
-      id: '_Default',
-      slice_type: 'text_image',
-    }}
-  />
-)
+
+export const _Default = () => <MyComponent slice={{"variation":"default","version":"sktwi1xtmkfgx8626","items":[{}],"primary":{"content":[{"type":"paragraph","text":"Eiusmod adipisicing aliquip veniam occaecat nisi elit laboris nostrud ea velit consequat enim sunt laboris.","spans":[]}]},"id":"_Default","slice_type":"text_image"}} />
 _Default.storyName = ''
