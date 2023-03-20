60a2d2cb37ac163f0786fbf2f1f645ed0d50a159
leoobarbosa2 <leonardobarbosa@MacBook-Pro-de-Leonardo.local>
2023-03-20 19:23:40 -0300

    feat: Removing bottom margin from default layout

--- src/templates/default/styles.ts
+++ src/templates/default/styles.ts
@@ -3,5 +3,5 @@ import styled from 'styled-components'
 export const Container = styled.div`
   max-width: 1080px;
   padding: 1.25rem;
-  margin: 1.25rem auto 3rem auto;
+  margin: 1.25rem auto 0 auto;
 `
