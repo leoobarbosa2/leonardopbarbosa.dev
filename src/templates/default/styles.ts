7f6e7dd5da829415a8d9aa54ef3deb8167db67be
leoobarbosa2 <leonardobarbosa@MacBook-Pro-de-Leonardo.local>
2023-03-20 21:04:55 -0300

    feat: fix mobile margin spacement at home page

--- src/templates/default/styles.ts
+++ src/templates/default/styles.ts
@@ -4,4 +4,8 @@ export const Container = styled.div`
   max-width: 1080px;
   padding: 1.25rem;
   margin: 1.25rem auto 0 auto;
+
+  @media (max-width: 800px) {
+    margin: 1.25rem auto 3rem auto;
+  }
 `
